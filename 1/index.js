eb5d4c539c24b2710bf068c1efafeaa45649b389
Bachhihi <80698524+Bachhihi@users.noreply.github.com>
2021-03-22 07:31:01 +0700

    goo

---
+++ 1/index.js
@@ -0,0 +1,5 @@
+alert("Chào mừng bạn đến với website")
+
+let age = prompt("Vui lòng nhập số tuổi")
+let yob = 2021 - Number(age)
+alert("Xin chào, năm nay bạn " + age + " tuổi, bạn sinh năm " + yob)
---
+++ 2/index.js
@@ -0,0 +1,82 @@
+// let i = 50
+// for ( i; i < 100; i+=2) {
+//     console.log("Số chẳn" + i)
+// }
+
+
+// let i = 50
+// while (i < 100) {
+//     console.log(i)
+//         (a += 1)
+// }
+
+// let n = prompt("Nhập số")
+// for (let i = 0; i < n; i += 4) {
+//     console.log(n + "Chia hết cho 4")
+// }
+
+// for (let i = 50; i < 100; i++) {
+//     if (i % 2 == 0) {
+//         console.log(i)
+//     }
+// }
+
+// let userNumber = prompt("Nhập số");
+// for (let i = 0; i <= userNumber; i++) {
+//     if (i % 4 == 0) {
+//         console.log(i)
+//     }
+// }
+
+// let myArray = [1,3,5,7,9]
+// // READ
+// // READ whole array
+// console.log(myArray)
+// // READ an element
+// console.log(myArray[1])
+// // READ length of Array
+// console.log(myArray.length)
+
+// // UPDATE
+// myArray[0] = 2
+// myArray[1] = 4
+// myArray[2] = 6
+// myArray[3] = 8
+// myArray[4] = 10
+// console.log(myArray)
+
+// // DELETE
+// myArray.pop()
+// console.log(myArray)
+// myArray.shift()
+// console.log(myArray)
+// myArray.splice(1,1,20)
+// console.log(myArray)
+
+
+// // ADD
+// // ADD vào cuối 
+// myArray.push(12)
+// myArray.push(14)
+// console.log(myArray)
+
+// let myArray = [1, 3, 5, 7, 9]
+// for (let i = 0; i < myArray.length; ++i) {
+//     if (i % 2 == 1) {
+//         myArray[i] = myArray[i] + 1
+//     }
+//     console.log(myArray[i])
+// }
+//  số index lẻ thì thêm 1 đơn vị
+
+// let userAge = prompt("Nhập tuổi")
+// if (userAge) {
+//     if (userAge < 10) {
+//         alert("Bạn là em bé")
+//     } else if (userAge < 18) {
+//         alert("Bạn là thiến niên")
+//     }
+// } else {
+//     alert("Bạn là người già")
+// }
+
